c43ea2eaba90923c6255c53996b77a1158ffda0a
JohanWL12 <johanwoel@gmail.com>
2022-12-20 17:18:19 +0300

    push ws

---
+++ src/test/java/com/example/gestion_flotte/GestionFlotteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_flotte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFlotteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
